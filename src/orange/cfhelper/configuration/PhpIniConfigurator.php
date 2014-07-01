d413d446ea8b72f7d61e6db4618a56a47b00e52c
Arthur Halet <arthur.halet@orange.com>
2014-07-01 15:25:46 +0200

    Add development value type to set in debug mode

--- src/orange/cfhelper/configuration/PhpIniConfigurator.php
+++ src/orange/cfhelper/configuration/PhpIniConfigurator.php
@@ -17,10 +17,38 @@ class PhpIniConfigurator
     public static $servicePhpIniName = 'php-ini';
     private $serviceManager;
     private $applicationInfo;
+    private $config = array();
 
     function __construct()
     {
+        $this->loadConfigCfHelper();
+    }
 
+    public function loadConfigCfHelper()
+    {
+        if (!is_file(__DIR__ . '/../../../../../../../composer.json')) {
+            return;
+        }
+        $composerJson = json_decode(file_get_contents(__DIR__ . '/../../../../../../../composer.json'), true);
+        if (empty($composerJson['cfhelper'])) {
+            return;
+        }
+        $this->config = $composerJson['cfhelper'];
+        $this->loadConfig();
+    }
+
+    public function loadConfig()
+    {
+        if (!empty($this->config['type']) && $this->config['type'] == 'development') {
+            ini_set("display_errors", "On");
+            ini_set("error_reporting", -1);
+            $whoops = new \Whoops\Run;
+            $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+            $whoops->register();
+        } else {
+            ini_set("display_errors", "Off");
+            ini_set("error_reporting", E_ALL & ~E_DEPRECATED);
+        }
     }
 
     public function loadIniConfig()
